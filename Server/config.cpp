ebfa7cd4cade6e48ef5988cfc89fce0f9de03294
SinyorDes <luen.eth@gmail.com>
2023-09-09 23:01:51 +0300

    config change

---
+++ Server/config.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+
+namespace Config {
+    const std::string DB_HOST = "localhost";
+    const std::string DB_USER = "myuser";
+    const std::string DB_PASSWORD = "mypassword";
+    const std::string DB_NAME = "mydatabase";
+}
